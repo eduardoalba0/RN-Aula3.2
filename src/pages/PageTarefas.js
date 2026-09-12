9d7459ebdb31e784dbcfe73a7bc1efb69ffe2102
Eduardo Alba <eduardoalba0@hotmail.com>
2026-09-12 12:00:04 -0300

    Manipulação do estado de Carregamento e RefreshControl

--- src/pages/PageTarefas.js
+++ src/pages/PageTarefas.js
@@ -1,17 +1,23 @@
-import { useState, useEffect } from "react"
+import { useState, useEffect } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
-import { StyleSheet } from "react-native"
+import { ActivityIndicator, FlatList, RefreshControl, StyleSheet, View } from "react-native";
 import { Card, Text } from "react-native-paper";
 
 function PageTarefas() {
+    const [carregando, setCarregando] = useState(false)
     const [tarefas, setTarefas] = useState([])
 
     async function listarTarefas() {
+        setCarregando(true)
         fetch("https://dummyjson.com/todos").then(response => {
             response.json().then(dados => {
-                setTarefas(dados.todos)
+                setTimeout(() => {
+                    setTarefas(dados.todos)
+                    setCarregando(false)
+                }, 2000)
             })
         }).catch(err => {
+            setCarregando(false)
             alert("Erro ao listar tarefas!")
         })
     }
@@ -22,17 +28,25 @@ function PageTarefas() {
 
     return (
         <SafeAreaView style={styles.container}>
-            {tarefas.map(tarefa => (
-                <Card>
-                    <Card.Content>
-                        <Text>{tarefa.todo}</Text>
-                    </Card.Content>
-                </Card>
-            ))}
+            <FlatList
+                data={tarefas}
+                keyExtractor={(tarefa) => tarefa.id}
+                renderItem={({ item }) => (
+                    <Card key={item.id}>
+                        <Card.Content>
+                            <Text>{item.todo}</Text>
+                        </Card.Content>
+                    </Card>
+                )}
+                refreshControl={
+                    <RefreshControl
+                        refreshing={carregando}
+                        onRefresh={listarTarefas}
+                    />
+                }
+            />
         </SafeAreaView>
     )
-
-
 }
 
 const styles = StyleSheet.create({
